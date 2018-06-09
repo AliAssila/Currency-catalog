085dd2694c205801080f0c277d65776483400b46
AliAssila <ali.assila@dhm-it.com>
2018-06-09 03:31:07 +0200

    patch 1

--- src/app/Currency/currency-detail/currency-detail.component.ts
+++ src/app/Currency/currency-detail/currency-detail.component.ts
@@ -1,6 +1,5 @@
 import {Component, OnDestroy, OnInit} from '@angular/core';
 import {ActivatedRoute, Router} from '@angular/router';
-import {Subscription} from 'rxjs/Subscription';
 import {Currency} from '../../shared/model/currency.model';
 import {CurrencyService} from '../currency.service';
 import {HttpResponse} from '@angular/common/http';
@@ -12,11 +11,12 @@ import {HttpResponse} from '@angular/common/http';
 })
 export class CurrencyDetailComponent implements OnInit, OnDestroy {
   pageTitle = 'Currency Detail';
-  private subscription: Subscription;
+  private subscription: any;
   currency: Currency;
   constructor(private _route: ActivatedRoute,
               private _router: Router,
-              private _currentService: CurrencyService) { }
+              private _currentService: CurrencyService,
+              ) { }
 
   ngOnInit() {
     this.subscription = this._route.params.subscribe((params) => {
